4254d1c3d9e065045a59aaa0988828945c983645
topwebdev0612 <81346046+topwebdev0612@users.noreply.github.com>
2021-04-12 11:03:11 -0700

    scrollable
    before new fragment

--- app/src/main/java/com/example/myapplication/ui/broadcast/BroadcastFragment.java
+++ app/src/main/java/com/example/myapplication/ui/broadcast/BroadcastFragment.java
@@ -28,30 +28,7 @@ public class BroadcastFragment extends Fragment {
                 new ViewModelProvider(this).get(BroadcastViewModel.class);
         View root = inflater.inflate(R.layout.fragment_broadcast, container, false);
 
-        final TextView textView = root.findViewById(R.id.text_broadcast);
-        ListView simpleListView = (ListView)root.findViewById(R.id.list_view) ;
-
-        int[] music_action = {R.drawable.music_action, R.drawable.music_action, R.drawable.music_action, R.drawable.music_action, R.drawable.music_action};
-        String[] band_name = {"Apple", "Banana", "Litchi", "Mango", "PineApple"};//fruit names array
-        String[] title = {"Apple", "Banana", "Litchi", "Mango", "PineApple"};
-        String[] description = {"Apple", "Banana", "Litchi", "Mango", "PineApple"};
-        int[] listener = {450, 450, 450, 450, 450};
-        String[] gen = {"Apple", "Banana", "Litchi", "Mango", "PineApple"};
-
-
-        ArrayList<HashMap<String,Object>> arrayList=new ArrayList<>();
-        for (int i=0;i<band_name.length;i++)
-        {
-            HashMap<String,Object> hashMap=new HashMap<>();//create a hashmap to store the data in key value pair
-            hashMap.put("band_name",band_name[i]);
-            hashMap.put("title",title[i]);
-            hashMap.put("music_action",music_action[i]+"");
-            hashMap.put("description", description[i]);
-            hashMap.put("listener", listener[i]);
-            arrayList.add(hashMap);//add the hashmap into arrayList
-        }
-        String[] from={"band_name","title","music_action","description","listener"};
-        int[] to={R.id.textView,R.id.imageView};
+//        final TextView textView = root.findViewById(R.id.text_broadcast);
 
 
 //        broadcastViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
--- app/src/main/java/com/example/myapplication/ui/listen/ListenFragment.java
+++ app/src/main/java/com/example/myapplication/ui/listen/ListenFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,6 +16,9 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.myapplication.R;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class ListenFragment extends Fragment {
 
     private ListenViewModel listenViewModel;
@@ -23,13 +28,40 @@ public class ListenFragment extends Fragment {
         listenViewModel =
                 new ViewModelProvider(this).get(ListenViewModel.class);
         View root = inflater.inflate(R.layout.fragment_listen, container, false);
+
+        ListView simpleListView = (ListView)root.findViewById(R.id.list_view) ;
+
+        int[] music_action = {R.drawable.music_action, R.drawable.music_action, R.drawable.music_action, R.drawable.music_action, R.drawable.music_action, R.drawable.logo, R.drawable.logo};
+        String[] band_name = {"Apple", "Banana", "Litchi", "Mango", "PineApple", "PineApple", "PineApple"};//fruit names array
+        String[] title = {"Apple", "Banana", "Litchi", "Mango", "PineApple", "PineApple", "PineApple"};
+        String[] description = {"Apple", "Banana", "Litchi", "Mango", "PineApple", "PineApple", "PineApple"};
+        int[] listener = {450, 450, 450, 450, 450, 450, 450};
+        String[] gen = {"Apple", "Banana", "Litchi", "Mango", "PineApple", "PineApple", "PineApple"};
+
+
+        ArrayList<HashMap<String,Object>> arrayList=new ArrayList<>();
+        for (int i=0;i<band_name.length;i++)
+        {
+            HashMap<String,Object> hashMap=new HashMap<>();//create a hashmap to store the data in key value pair
+            hashMap.put("band_name",band_name[i]);
+            hashMap.put("title",title[i]);
+            hashMap.put("music_action",music_action[i]+"");
+            hashMap.put("description", description[i]);
+            hashMap.put("listener", listener[i]);
+            hashMap.put("gen", gen[i]);
+            arrayList.add(hashMap);//add the hashmap into arrayList
+        }
+        String[] from={"band_name","title","music_action","description","listener","gen"};
+        int[] to={R.id.band_name,R.id.title,R.id.imageView,R.id.description,R.id.listener,R.id.gen};
+        SimpleAdapter simpleAdapter=new SimpleAdapter(this.getContext(),arrayList,R.layout.list_view_items,from,to);//Create object and set the parameters for simpleAdapter
+        simpleListView.setAdapter(simpleAdapter);//sets the adapter for listView
 //        final TextView textView = root.findViewById(R.id.text_listen);
-        listenViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-            }
-        });
+//        listenViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+////                textView.setText(s);
+//            }
+//        });
         return root;
     }
 }
